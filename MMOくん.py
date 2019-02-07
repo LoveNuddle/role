ee53d6c77e4f707f0937d1b1935a89d878593a27
MMOKUN <46297678+MMOKUN@users.noreply.github.com>
2019-02-07 19:41:30 +0900

    Update MMOくん.py

--- MMOくん.py
+++ MMOくん.py
@@ -1726,4 +1726,4 @@ async def on_message(message: discord.Message):
             # -------------------------------------------------------------------------------------------------------------------
 
 client.loop.create_task(change_status())
-client.run(os.environ.get("TOKEN"))
+client.run(os.environ.get("TOKEN")
